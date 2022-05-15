d13218200a1dddf9d69cf85e9612d483b95fbf43
André L. C. Moreira <alcm99@gmail.com>
2022-05-14 23:22:19 -0300

    adding macro to generate hooked functions

--- lib/preload.c
+++ lib/preload.c
@@ -4,26 +4,28 @@
 
 #include <dlfcn.h>
 
+#define GENERATE_HOOK(ret, name, params_sig, param_types, param_values) \
+  ret name params_sig { \
+    if (mocked_##name) { \
+      return mocked_##name param_values; \
+    } \
+    printf("executing default action\n"); \
+    void *_##name_ptr = dlsym(RTLD_NEXT, #name); \
+    if (_##name_ptr) { \
+      ret (*real_##name) param_types = \
+        (ret (*) param_types)_##name_ptr; \
+      return real_##name param_values; \
+    } \
+    return 0; \
+  }
+
 ssize_t (*mocked_write)(int, const void *, size_t);
 
 void set_write(ssize_t (*write_ptr)(int, const void *, size_t)) {
   mocked_write = write_ptr;
 }
 
-ssize_t write(int fd, const void *buffer, size_t length) {
-  if (mocked_write) {
-    return mocked_write(fd, buffer, length);
-  }
-
-  printf("executing default action\n");
-
-  void *write_ptr = dlsym(RTLD_NEXT, "write");
-
-  if (write_ptr) {
-    ssize_t (*real_write)(int, const void *, size_t) =
-      (ssize_t (*)(int, const void *, size_t))write_ptr;
-    return real_write(fd, buffer, length);
-  }
-
-  return 0;
-}
+GENERATE_HOOK(ssize_t, write,
+              (int fd, const void *buffer, size_t length),
+              (int, const void *, size_t),
+              (fd, buffer, length));
